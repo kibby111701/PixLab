77d0bd6fb60c598d9ab02b8e3e49125a766124ce
Christopher Michael Kraft <kraftcm@s.dcsdk12.org>
2020-02-11 09:43:38 -0700

    make getcount

--- src/IntArrayWorker.java
+++ src/IntArrayWorker.java
@@ -85,4 +85,16 @@ public class IntArrayWorker {
 		}
 	}
 
+	public int getCount(int num){
+		int count = 0;
+		for (int row = 0; row < matrix.length; row++){
+			for (int col = 0; col < matrix[0].length; col++){
+				if (matrix[row][col] == num){
+					count += 1;
+				}
+			}
+		}
+		return count;
+	}
+
 }
